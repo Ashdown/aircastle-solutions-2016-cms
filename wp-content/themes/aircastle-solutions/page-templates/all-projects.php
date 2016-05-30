95145329903e4a97d4fd2638ca556080a3cb3552
Rory Devane <rory.devane@gmail.com>
2016-05-30 17:15:17 +0100

    updated all projects template

--- wp-content/themes/aircastle-solutions/page-templates/all-projects.php
+++ wp-content/themes/aircastle-solutions/page-templates/all-projects.php
@@ -1,16 +1,19 @@
 <?php /* Template Name: All Projects Template */ ?>
 
-<?php
-$results = array(
-    'one' => array(
-        'title' => 'First Title',
-        'description' => 'Lorem ipsum dolar sit amet'
-    ),
-    'two' => array(
-        'title' => 'Second Title',
-        'description' => 'Lorem ipsum dolar sit amet'
-    )
-);
+<h1>All Projects Template</h1>
+<p>All projects template</p>
 
-echo json_encode($results);
+<?php
+//$results = array(
+//    'one' => array(
+//        'title' => 'First Title',
+//        'description' => 'Lorem ipsum dolar sit amet'
+//    ),
+//    'two' => array(
+//        'title' => 'Second Title',
+//        'description' => 'Lorem ipsum dolar sit amet'
+//    )
+//);
+//
+//echo json_encode($results);
 ?>

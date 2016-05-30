696b1e5191e48417ee21e141554c71f705c71c71
Rory Devane <rory.devane@gmail.com>
2016-05-30 17:23:07 +0100

    removed header and footer fro all projects page

--- wp-content/themes/aircastle-solutions/page-templates/all-projects.php
+++ wp-content/themes/aircastle-solutions/page-templates/all-projects.php
@@ -1,9 +1,6 @@
 <?php /* Template Name: All Projects Template */ ?>
 
-<?php get_header(); ?>
 
-    <h1>Item Page</h1>
-    <p>This is a sample item page</p>
+<h1>All projects template</h1>
+<p>This is a sample item page</p>
 
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>

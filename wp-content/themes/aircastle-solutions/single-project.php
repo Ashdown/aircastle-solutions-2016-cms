d275df5be6e80a7bf41dfb1a7579cd6b3d805e37
Rory Devane <rory.devane@gmail.com>
2017-01-04 11:47:30 +0000

    updated single project template with real values

--- wp-content/themes/aircastle-solutions/single-project.php
+++ wp-content/themes/aircastle-solutions/single-project.php
@@ -1,3 +1,31 @@
 {
-    "test": "Single Project Template"
+    "id": "<?php the_ID(); ?>",
+    "name": "<?php echo get_the_title(); ?>",
+    <?php $picture = get_field('picture'); ?>
+    "picture": {
+    "url": "<?php echo $picture['url'] ?>",
+    "alt": "<?php echo $picture['alt'] ?>",
+    "height": "<?php echo $picture['sizes']['full-height'] ?>",
+    "width": "<?php echo $picture['sizes']['full-width'] ?>",
+    },
+    "description": "<?php echo get_field('description'); ?>",
+    <?php if(have_rows('links')): ?>
+        "links":[
+        <?php while(have_rows('links')) : the_row(); ?>
+            {
+            "text": "<?php the_sub_field('text'); ?>",
+            "url":  "<?php the_sub_field('url'); ?>"
+            },
+        <?php endwhile; ?>
+        ],
+    <?php endif; ?>
+    <?php if(have_rows('keywords')): ?>
+        "keywords": [
+        <?php while(have_rows('keywords')) : the_row(); ?>
+            "<?php the_sub_field('keyword'); ?>",
+        <?php endwhile; ?>
+    <?php endif; ?>
+    "startDate": "sample",
+    "endDate": "sample"
 }
+

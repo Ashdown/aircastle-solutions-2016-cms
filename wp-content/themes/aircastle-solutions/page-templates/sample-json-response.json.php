00324e9344c0ad5307a99431b496f6681c0a748e
Rory Devane <rory.devane@gmail.com>
2017-01-04 11:36:11 +0000

    added real values for id, name, picture, description, links, and keywords

--- wp-content/themes/aircastle-solutions/page-templates/sample-json-response.json.php
+++ wp-content/themes/aircastle-solutions/page-templates/sample-json-response.json.php
@@ -1,7 +1,38 @@
-<?php /* Template Name: Sample Json Response */ ?>
+<?php
+
+/* Template Name: Sample Json Response */
+
+$picture = get_field('picture');
+
+?>
 
 {
-    "id": 1,
-    "pageName": "Sample json reponse page"
+    "id": "<?php the_ID(); ?>",
+    "name": "<?php echo get_the_title(); ?>",
+    "picture": {
+        "url": "<?php echo $picture['url'] ?>",
+        "alt": "<?php echo $picture['alt'] ?>",
+        "height": "<?php echo $picture['sizes']['full-height'] ?>",
+        "width": "<?php echo $picture['sizes']['full-width'] ?>",
+    },
+    "description": "<?php echo get_field('description'); ?>",
+    <?php if(have_rows('links')): ?>
+    "links":[
+        <?php while(have_rows('links')) : the_row(); ?>
+            {
+                "text": "<?php the_sub_field('text'); ?>",
+                "url":  "<?php the_sub_field('url'); ?>"
+            },
+        <?php endwhile; ?>
+    ],
+    <?php endif; ?>
+    <?php if(have_rows('keywords')): ?>
+    "keywords": [
+        <?php while(have_rows('keywords')) : the_row(); ?>
+            "<?php the_sub_field('keyword'); ?>",
+        <?php endwhile; ?>
+    <?php endif; ?>
+    "startDate": "sample",
+    "endDate": "sample"
 }
 

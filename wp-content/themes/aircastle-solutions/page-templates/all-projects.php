eab1f45ecad1cbf7897786929e83c7cb1b0819e5
Rory Devane <rory.devane@gmail.com>
2017-01-04 15:07:49 +0000

    removed unused template files

--- wp-content/themes/aircastle-solutions/page-templates/all-projects.php
+++
@@ -1,9 +0,0 @@
-<?php /* Template Name: All Projects Template */ ?>
-
-<?php get_header(); ?>
-
-    <h1>Item Page</h1>
-    <p>This is a sample item page</p>
-
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>

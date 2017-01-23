21352e9cf96ecf27143a6b0e82fc005fc26e3fdc
Rory Devane <rory.devane@gmail.com>
2017-01-23 12:43:02 +0000

    updated picture property to array

--- wp-content/themes/aircastle-solutions/single-project.php
+++ wp-content/themes/aircastle-solutions/single-project.php
@@ -6,14 +6,25 @@ $count = 0;
     "id": "<?php the_ID(); ?>",
     "name": "<?php echo get_the_title(); ?>",
     "type": "<?php the_field('type'); ?>",
-    <?php $picture = get_field('picture'); ?>
-
-    "picture": {
-        "url": "<?php echo $picture['url'] ?>",
-        "alt": "<?php echo $picture['alt'] ?>",
-        "height": "<?php echo $picture['sizes']['large-height'] ?>",
-        "width": "<?php echo $picture['sizes']['large-width'] ?>"
-    },
+    <?php if(have_rows('pictures')): ?>
+        "pictures" : [
+        <?php
+        while(have_rows('pictures')) :
+            the_row();
+            $delimiter = (++$count == count(get_field('pictures'))) ? '' : ',';
+            $picture = get_sub_field('picture');
+            ?>
+            {
+                "url": "<?php echo $picture['url'] ?>",
+                "alt": "<?php echo $picture['alt'] ?>",
+                "height": "<?php echo $picture['sizes']['large-height'] ?>",
+                "width": "<?php echo $picture['sizes']['large-width'] ?>"
+            }<?php echo $delimiter; ?>
+            <?php
+        endwhile;
+        ?>
+        ],
+    <?php endif; ?>
     "description": "<?php echo get_field('description'); ?>",
     <?php if(have_rows('links')): ?>
         "links":[

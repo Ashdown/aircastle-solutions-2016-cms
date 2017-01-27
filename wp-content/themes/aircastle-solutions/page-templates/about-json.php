e911e7ca64daf7f8e0eb3ff9803806a56866e562
Rory Devane <rory.devane@gmail.com>
2017-01-27 17:15:22 +0000

    added about page template

---
+++ wp-content/themes/aircastle-solutions/page-templates/about-json.php
@@ -0,0 +1,19 @@
+<?php /* Template Name: About Data */ ?>
+
+<?php
+$callback = preg_replace('/[^-a-zA-Z0-9_]/', '', $_GET['callback']);
+$picture = get_field('picture');
+$url = str_replace('shielded-scrubland-59117.herokuapp.com', 'cms.rorydevane.com', $picture['url']);
+?>
+
+<?php echo $callback; ?>({
+{
+    "picture": {
+        "url": "<?php echo $url ?>",
+        "alt": "<?php echo $picture['alt'] ?>",
+        "height": "<?php echo $picture['sizes']['large-height'] ?>",
+        "width": "<?php echo $picture['sizes']['large-width'] ?>"
+    },
+    "description": "<?php get_the_field('description') ?>"
+    }
+});

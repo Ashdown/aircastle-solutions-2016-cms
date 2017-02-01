646133d5337c2e1e8a3a3550f40376f9ba5ec6fa
Rory Devane <rory.devane@gmail.com>
2017-02-01 00:17:17 +0000

    changed date format

--- wp-content/themes/aircastle-solutions/archive-project.php
+++ wp-content/themes/aircastle-solutions/archive-project.php
@@ -76,8 +76,8 @@
             $startDate = DateTime::createFromFormat('d/m/Y', get_field('start_date'));
             $endDate = DateTime::createFromFormat('d/m/Y', get_field('end_date'));
             ?>
-            "startDate": "<?php echo $startDate->format('F, Y'); ?>",
-            "endDate": "<?php echo $endDate->format('F, Y'); ?>",
+            "startDate": "<?php echo $startDate->format('YYYY-MM-DD'); ?>",
+            "endDate": "<?php echo $endDate->format('YYYY-MM-DD'); ?>",
             "url": "/projects/<?php echo basename(get_permalink()); ?>"
         }<?php echo $deliminator; ?>
         <?php
